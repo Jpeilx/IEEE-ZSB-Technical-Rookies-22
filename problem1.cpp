20be3f90de54eba18204d2d02cb7f1710f439ee8
Jpeilx <96302371+Jpeilx@users.noreply.github.com>
2021-12-17 17:42:16 +0200

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int l , w ;
+    cin>>l>>w ;
+    cout<<l*w<<"\n"<<(l+w)*2 ;
+    return 0 ;
+}
+
