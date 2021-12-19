c3c493f58712726c3e7b7d5f55ed5ec9f87aae87
Jpeilx <96302371+Jpeilx@users.noreply.github.com>
2021-12-19 14:37:54 +0200

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    string s ; int max =0  , last =0 , now = -1 ;
+    getline(cin , s ) ;
+  for(int i =0 ;i<s.length();i++) {
+      now ++ ;
+      if (s[i]== ' ') {
+          last = now ;
+          now =-1 ;}
+      if (max <last )
+          max=last ;
+  }
+  int x = 1 ;
+  for (int i = 0 ; i<max+2 ; i++)
+      cout<<"*" ;
+    cout<<"\n" ;
+    cout<<'*' ;
+   for (int j =0 ; j<s.length() ; j++){
+       if (j==s.length()-1&&s[j]!=' '){
+           cout<<s[s.length()-1] ;
+           for (int i=0 ; i<max-x ; i++)
+               cout<<' ' ;
+           cout<<"*" ;
+           break;
+
+       }
+      else if (s[j]==' '  ){
+          for (int i=0 ; i<max-x+1 ; i++)
+              cout<<' ' ;
+          if (j==s.length()-1)
+              cout<<"*" ;
+          else
+              cout<<"*\n*";
+          x=1;
+          j++ ;
+
+      }
+      x++ ;
+      cout<<s[j] ;
+
+   }
+
+    cout<<"\n" ;
+    for (int i = 0 ; i<max+2 ; i++)
+        cout<<"*" ;
+
+
+}
