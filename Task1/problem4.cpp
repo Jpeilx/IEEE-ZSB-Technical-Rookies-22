7a6c5c10809ebe08efea692a31482808ee8f49d7
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2021-12-21 19:15:24 +0200

    Update problem4.cpp

--- Task1/problem4.cpp
+++ Task1/problem4.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
 using namespace std;
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
-    string s ; int max =0  , last =0 , now = -1 ;
+    string s ; int max =0  , last =0 , now = 1 ;
     getline(cin , s ) ;
   for(int i =0 ;i<s.length();i++) {
       now ++ ;
       if (s[i]== ' ') {
           last = now ;
-          now =-1 ;}
+          now = 1 ;}
       if (max <last )
           max=last ;
   }
@@ -19,8 +18,9 @@ int main() {
   for (int i = 0 ; i<max+2 ; i++)
       cout<<"*" ;
     cout<<"\n" ;
-    cout<<'*' ;
+    cout<<"* " ;
    for (int j =0 ; j<s.length() ; j++){
+
        if (j==s.length()-1&&s[j]!=' '){
            cout<<s[s.length()-1] ;
            for (int i=0 ; i<max-x ; i++)
@@ -30,14 +30,16 @@ int main() {
 
        }
       else if (s[j]==' '  ){
-          for (int i=0 ; i<max-x+1 ; i++)
+           if (x-1==j)
+               x++ ;
+          for (int i=0 ; i<max-x +1 ; i++)
               cout<<' ' ;
           if (j==s.length()-1)
               cout<<"*" ;
           else
               cout<<"*\n*";
           x=1;
-          j++ ;
+
 
       }
       x++ ;
