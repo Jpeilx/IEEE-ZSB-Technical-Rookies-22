7ffdb968c8d193cf06ba07c14fec184140ad563e
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2021-12-22 20:29:55 +0200

    Create problem3.cpp

---
+++ Task2/problem3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int guss_game2(int t);
+#define ll  long long
+#define v  vector<int>
+#define l list<int>
+#define all(x) x.begin(), x.end()
+#define xx(i,a,n) for (int i = a ; i<n ; i++)
+int main () {
+    srand(time(NULL)) ;
+    int t = rand()%898+100 ;
+    int tr = guss_game2(t) ;
+    cout<<"you take "<<tr<<"to win" ;
+
+}
+int guss_game2(int t) {
+    int miss =0 ,  hit = 0 , tries = 0  ;
+    int n , x ;
+    x=t ;
+    cout<<"Enter a number has 3 digits \n";
+    cin>>n ;
+    v vn(3) ,vx(3)  ;
+    xx(i,0,3){
+     vn[i]=n%10 ;
+     vx[i]=x%10 ;
+     n/=10 ;
+     x/=10 ;
+    }
+    int y =3 ;
+    xx(i,0,y){
+        if (vn[i]==vx[i]){
+            hit++;
+            vx.erase(vx.begin()+i) ;
+            vn.erase(vn.begin()+i) ;
+            i-- ; y-- ;
+        }
+    }
+    if (hit==3)
+        return hit ;
+    xx(i,0,y){
+
+            xx(j,0,y){
+            if (vn[i]==vx[j])
+                    miss++;
+
+
+        }
+       }
+
+        cout<<hit<<"hit "<<miss<<" miss"<<endl  ;
+    return  guss_game2( t) ;
+
+}
